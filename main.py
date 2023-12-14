4ecfb38bb9c33d53cd03711ccaa7940c440e858b
xxxephyr <carlorf1@uci.edu>
2023-12-13 22:43:29 -0800

    first committ

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # Hi
 from ucimlrepo import fetch_ucirepo 
 import numpy as np
-import pandas as pd
 
 # fetch dataset 
 diabetes_130_us_hospitals_for_years_1999_2008 = fetch_ucirepo(id=296) 
@@ -9,6 +8,12 @@ diabetes_130_us_hospitals_for_years_1999_2008 = fetch_ucirepo(id=296)
 # data (as pandas dataframes) 
 X = diabetes_130_us_hospitals_for_years_1999_2008.data.features 
 y = diabetes_130_us_hospitals_for_years_1999_2008.data.targets 
+
+# for i in X:
+#     column = eval(f"X.{i}")    #Each column has ID and Feature
+#     #print(column)
+
+
 print(X.shape)
 print(y.shape)
 # metadata 
